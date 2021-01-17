79fe449a66d12e66c5269ecc1affe1fe3d17da79
Cora <cora.ottaviani.berlin@gmail.com>
2021-01-17 11:28:47 -0500

    Set up gh-pages

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
+  siteUrl: 'https://eco-fable.github.io',
   siteName: 'Gridsome Blog Starter',
   siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.',
 
