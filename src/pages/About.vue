5bbe339f6b5cee223edd9396b584913bb7165114
Cora <cora.ottaviani.berlin@gmail.com>
2021-05-01 15:32:16 -0400

    Wrap about pg in Layout component

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="author-info">
-    <h1>About the Author</h1>
-    <p>I’m a twenty-something trying to find her place in this vast, scary, and sometimes wonderous universe. I have a Master in Environmental Management, and have worked with state agencies and a variety of environmental nonprofits. I’m interested in wildlife conservation, habitat management, and environmental communication. I have experience working in the field using camera traps, conducting water quality tests, and native/invasive vegetation assessments. I’ve also analyzed data using software/programs like ArcGIS, QGIS, Circuitscape, Maxent, and R.</p>
-    <p>
-      I enjoy reading, art history, learning about wildlife, folklore/mythology, and writing.
-      I am currently working as a state seasonal technician focusing on forest health.
-    </p>
-    <p>I also work nights managing New England’s largest collection of vampire hunting memorabilia.</p>
-    <p>Just kidding.</p>
-    <p>I’m actually a freelance crypto-consultant. I spent this past fall tracking Connecticut’s elusive Glawackus, and am now helping a neighbor negotiate rent for a family of squatting fairies in her attic.</p>
-    <p>No but seriously.</p>
-    <p>I work as a blacksmith, forging weapons for the eventual zombie apocalypse.</p>
-  </div>
+  <Layout>
+    <div class="author-info">
+      <h1>About the Author</h1>
+      <p>I’m a twenty-something trying to find her place in this vast, scary, and sometimes wonderous universe. I have a Master in Environmental Management, and have worked with state agencies and a variety of environmental nonprofits. I’m interested in wildlife conservation, habitat management, and environmental communication. I have experience working in the field using camera traps, conducting water quality tests, and native/invasive vegetation assessments. I’ve also analyzed data using software/programs like ArcGIS, QGIS, Circuitscape, Maxent, and R.</p>
+      <p>
+        I enjoy reading, art history, learning about wildlife, folklore/mythology, and writing.
+        I am currently working as a state seasonal technician focusing on forest health.
+      </p>
+      <p>I also work nights managing New England’s largest collection of vampire hunting memorabilia.</p>
+      <p>Just kidding.</p>
+      <p>I’m actually a freelance crypto-consultant. I spent this past fall tracking Connecticut’s elusive Glawackus, and am now helping a neighbor negotiate rent for a family of squatting fairies in her attic.</p>
+      <p>No but seriously.</p>
+      <p>I work as a blacksmith, forging weapons for the eventual zombie apocalypse.</p>
+    </div>
+  </Layout>
 </template>
 
 <static-query>
